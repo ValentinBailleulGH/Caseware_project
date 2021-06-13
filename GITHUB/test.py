449d8d030b6bafe99f14c76e083265c4052c0a9d
ValentinBailleulGH <85673619+ValentinBailleulGH@users.noreply.github.com>
2021-06-13 19:01:48 +0200

    Delete GITHUB directory

--- GITHUB/test.py
+++
@@ -1,7 +0,0 @@
-# python "src\test.py" "\resource\fichier_de_base_unit_testing(1).txt"
-
-import sys
-sys.path.append("..")
-print("!!!",sys.path)
-# print('Number of arguments: ', len(sys.argv), ' arguments')
-# print('Argument List: ', str(sys.argv))
